5e1fd3fb2b42eb4b0a007f11a99fc51ef4be23db
Michael Sturm <mikesturm2001@yahoo.com>
2023-10-11 16:01:38 -0500

    cleaning up permissions

--- terraform/live/dev/services/application/main.tf
+++ terraform/live/dev/services/application/main.tf
@@ -1,13 +1,3 @@
-# Fetch S3 bucket information
-data "terraform_remote_state" "s3" {
-  backend = "s3"
-  config = {
-    bucket = "terraform-data-movement-state-1247"
-    key    = "global/s3/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
 # Fetch EC2 Role information
 data "terraform_remote_state" "ec2_role" {
   backend = "s3"
@@ -30,29 +20,6 @@ module "data-movement" {
   max_size = 2
 }
 
-
-# Attach an inline policy to the IAM role to grant S3 permissions
-resource "aws_iam_policy" "s3_permissions_policy" {
-  name        = "s3-access-policy"
-  description = "Policy to access S3 buckets"
-  
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action   = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
-        Effect   = "Allow",
-        Resource = data.terraform_remote_state.s3.outputs.s3_bucket_arns
-      }
-    ]
-  })
-}
-
-# need to get ec2 role as well
-resource "aws_iam_role_policy_attachment" "s3_permissions_attachment" {
-  policy_arn = aws_iam_policy.s3_permissions_policy.arn
-  role       = data.terraform_remote_state.ec2_role.outputs.ec2_role_arn
-}
 
 # Create SNS topic
 resource "aws_sns_topic" "s3-landing-zone-sns-topic" {
--- terraform/live/global/iam/main.tf
+++ terraform/live/global/iam/main.tf
@@ -10,6 +10,16 @@ terraform {
   }
 }
 
+# Fetch S3 bucket information
+data "terraform_remote_state" "s3" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-data-movement-state-1247"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 # Create an IAM role for EC2 instances to assume
 resource "aws_iam_role" "ec2_role" {
   name = "ec2-s3-access-role"
@@ -28,6 +38,29 @@ resource "aws_iam_role" "ec2_role" {
   lifecycle {
     ignore_changes = [tags, assume_role_policy]  # Ignore changes to tags
   }
+}
+
+# Attach an inline policy to the IAM role to grant S3 permissions
+resource "aws_iam_policy" "s3_permissions_policy" {
+  name        = "s3-access-policy"
+  description = "Policy to access S3 buckets"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
+        Effect   = "Allow",
+        Resource = data.terraform_remote_state.s3.outputs.s3_bucket_arns
+      }
+    ]
+  })
+}
+
+# need to get ec2 role as well
+resource "aws_iam_role_policy_attachment" "s3_permissions_attachment" {
+  policy_arn = aws_iam_policy.s3_permissions_policy.arn
+  role       = aws_iam_role.ec2_role
 }
 
 # Create an instance profile for the EC2 instances
