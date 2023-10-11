8abf9bb4af4cd364257c4029b5205f1124a6f444
Michael Sturm <mikesturm2001@yahoo.com>
2023-10-11 09:59:40 -0500

    replacing count parameter loops with for_each

--- terraform/live/global/s3/main.tf
+++ terraform/live/global/s3/main.tf
@@ -1,15 +1,16 @@
 # Define a variable to hold the names of S3 buckets as a list
 variable "bucket_names" {
+  description = "List of S3 buckets used as drop zones for data movement"
   type    = list(string)
   default = ["s3-drop-zone-12134477a", "snowflake-drop-zone-12134477a"]
 }
 
 # Create S3 buckets using the variable
 resource "aws_s3_bucket" "s3_buckets" {
-  count = length(var.bucket_names)
+  for_each = toset(var.bucket_names)
 
-  bucket = var.bucket_names[count.index]
-  acl    = "private"
+  bucket = each.key
+  acl = "private"
 
   # Prevent Terraform from attempting to recreate an existing bucket
   lifecycle {
@@ -19,19 +20,20 @@ resource "aws_s3_bucket" "s3_buckets" {
 
 # Enable versioning, server-side encryption, and public access block for each S3 bucket
 resource "aws_s3_bucket_versioning" "enabled" {
-  count = length(var.bucket_names)
+  for_each = aws_s3_bucket.s3_buckets
 
-  bucket = aws_s3_bucket.s3_buckets[count.index].id
+  bucket = each.value.id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
+# Enable encryption at rest by default
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-  count = length(var.bucket_names)
+  for_each = aws_s3_bucket.s3_buckets
 
-  bucket = aws_s3_bucket.s3_buckets[count.index].id
+  bucket = each.value.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -40,10 +42,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   }
 }
 
+# Block public access to the S3 buckets
 resource "aws_s3_bucket_public_access_block" "public_access" {
-  count = length(var.bucket_names)
+  for_each = aws_s3_bucket.s3_buckets
 
-  bucket = aws_s3_bucket.s3_buckets[count.index].id
+  bucket = each.value.id
 
   block_public_acls       = true
   block_public_policy     = true
