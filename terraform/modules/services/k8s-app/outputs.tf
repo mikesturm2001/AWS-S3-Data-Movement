558fecac65ce3ba9dc73ab41b3b4036f5cdf5c86
Michael Sturm <mikesturm2001@yahoo.com>
2023-10-17 15:14:39 -0500

    Update outputs.tf

--- terraform/modules/services/k8s-app/outputs.tf
+++ terraform/modules/services/k8s-app/outputs.tf
@@ -1,1 +1,16 @@
-test
+output "service_status" {
+  value       = kubernetes_service.app.status
+  description = "The K8S Service status"
+}
+
+locals {
+  status = kubernetes_service.app.status
+}
+
+output "service_endpoint" {
+  value = try(
+    "http://${local.status[0]["load_balancer"][0]["ingress"][0]["hostname"]}",
+    "(error parsing hostname from status)"
+  )
+  description = "The K8S Service endpoint"
+}
