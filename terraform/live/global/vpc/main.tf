73590c18a732369f677e42fa295002a1532dff2c
Michael Sturm <mikesturm2001@yahoo.com>
2023-10-18 11:05:31 -0500

    adding natgateway to private subnets

--- terraform/live/global/vpc/main.tf
+++ terraform/live/global/vpc/main.tf
@@ -31,6 +31,10 @@ locals {
   }
 }
 
+resource "aws_eip" "nat_gateway_eip" {
+  instance = aws_nat_gateway.nat_gateway.id
+}
+
 resource "aws_subnet" "public_subnet" {
   count          = 1
   vpc_id         = aws_vpc.data_movement_vpc.id
@@ -38,6 +42,14 @@ resource "aws_subnet" "public_subnet" {
   availability_zone = "us-east-1${local.availability_zones[count.index]}"
   map_public_ip_on_launch = true
 }
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnet[0].id  # Associating with the public subnet
+
+  depends_on = [aws_subnet.public_subnet]  # Ensure the public subnet is created first
+}
+
 
 resource "aws_subnet" "private_subnets" {
   count = 2
@@ -47,4 +59,25 @@ resource "aws_subnet" "private_subnets" {
   tags = {
     Name = "private-subnet-${count.index + 1}"
   }
+}
+
+# Update the route tables to allow public subnets to communicate via the internet
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = 2
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_tables[count.index].id
+}
+
+# Add routes for private subnets to nat gateways
+resource "aws_route" "private_route" {
+  count           = 2
+  route_table_id  = aws_route_table.private_route_tables[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id  = aws_nat_gateway.nat_gateway.id
+}
+
+# Create private route tables
+resource "aws_route_table" "private_route_tables" {
+  count = 2
+  vpc_id = aws_vpc.data_movement_vpc.id
 }
