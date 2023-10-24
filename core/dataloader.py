ffc9eb40fde29fee79c7090c027cb25648a08b4a
Michael Sturm <mikesturm2001@yahoo.com>
2023-10-24 15:07:29 -0500

    s3 to s3 bucket cleanup

--- core/dataloader.py
+++ core/dataloader.py
@@ -1,12 +1,31 @@
 import boto3
 import os
 import debugpy
+import json
+from json import JSONDecodeError
+
+
+def decode_json_recursively(obj):
+    try:
+        if isinstance(obj, list):
+            data = [decode_json_recursively(el) for el in obj]
+        elif isinstance(obj, dict):
+            data = obj
+        else:
+            data = json.loads(obj)
+
+        if isinstance(data, dict):
+            for k, v in data.items():
+                data[k] = decode_json_recursively(v)
+    except (JSONDecodeError, TypeError, AttributeError):
+        return obj
+    return data
 
 
 def read_queue():
     
     # Set up the AWS S3 and SQS clients #need to wrap in error handling
-    s3_client = boto3.client('s3')
+    s3 = boto3.resource('s3')
     sqs_client = boto3.client('sqs', region_name='us-east-1')
 
     queue_url = os.environ.get("SQS_QUEUE_URL")
@@ -30,10 +49,22 @@ def read_queue():
                 for message in response['Messages']:
                     # Extract S3 notification details from the message
                     s3_event = message['Body']
-                    print("Received S3 Notification:")
-                    print(s3_event)
 
-                    # Process the S3 event as needed
+                    s3_event_dict = decode_json_recursively(s3_event)
+                    # Extract the object key
+                    object_key = s3_event_dict["Message"]["detail"]["object"]["key"]
+
+                    src_bucket = s3.Bucket(s3_drop_zone_bucket)
+                    src_obj = src_bucket.Object(object_key)
+
+                    drop_zone_file = {
+                        'Bucket': src_bucket.name,
+                        'Key': src_obj.key
+                    }
+
+                    s3.meta.client.copy(drop_zone_file, s3_snowflake_bucket, object_key)
+
+                    
 
                     # Delete the message from the SQS queue
                     receipt_handle = message['ReceiptHandle']
