8be917f7e61ef1a4860cf61f1a4a0dd750530172
Michael Sturm <mikesturm2001@yahoo.com>
2023-10-25 16:06:47 -0500

    removing duplicate variable

--- terraform/modules/services/ecs-cluster/variables.tf
+++ terraform/modules/services/ecs-cluster/variables.tf
@@ -28,11 +28,6 @@ variable "subnet_ids" {
   type = list(string)
 }
 
-variable "name" {
-    description = "The name to use for all resources created by this module"
-    type = string
-}
-
 variable "image" {
     description = "The Docker image to run"
     type = string
