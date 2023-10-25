86a1e9340e19787ca3e570f9eddee89360280cdd
Michael Sturm <mikesturm2001@yahoo.com>
2023-10-25 15:13:15 -0500

    updating outputs

--- terraform/live/global/s3/outputs.tf
+++ terraform/live/global/s3/outputs.tf
@@ -1,34 +1,39 @@
-output "s3_bucket_names" {
-  description = "Names of the S3 buckets"
-  value       = [for bucket_name in aws_s3_bucket.s3_buckets : bucket_name.bucket]
-}
-
 output "drop_zone_bucket" {
-  description = "Names of the S3 bucket for loading files to AWS"
+  description = "S3 bucket for loading files to AWS"
   value = aws_s3_bucket.s3-drop-zone
 }
 
+output "drop_zone_bucket_name" {
+  description = "Name of the S3 bucket for loading files to AWS"
+  value = aws_s3_bucket.s3-drop-zone.bucket
+}
+
+output "drop_zone_bucket_id" {
+  description = "Id of the S3 bucket for loading files to AWS"
+  value = aws_s3_bucket.s3-drop-zone.id
+}
+
 output "drop_zone_bucket_arn" {
-  description = "Names of the S3 bucket for loading files to AWS"
+  description = "ARN of the S3 bucket for loading files to AWS"
   value = aws_s3_bucket.s3-drop-zone.arn
 }
 
 output "snowflake_bucket" {
-  description = "Arn of the S3 bucket for loading files to snowflake"
+  description = "S3 bucket for loading files to snowflake"
   value = aws_s3_bucket.s3-snowflake-zone
 }
 
+output "snowflake_bucket_name" {
+  description = "Name of the S3 bucket for loading files to snowflake"
+  value = aws_s3_bucket.s3-snowflake-zone.bucket
+}
+
+output "snowflake_bucket_id" {
+  description = "Id of the S3 bucket for loading files to snowflake"
+  value = aws_s3_bucket.s3-snowflake-zone.id
+}
+
 output "snowflake_bucket_arn" {
-  description = "Names of the S3 bucket for loading files to snowflake"
+  description = "ARN of the S3 bucket for loading files to snowflake"
   value = aws_s3_bucket.s3-snowflake-zone.arn
-}
-
-output "s3_bucket_ids" {
-  description = "IDs of the S3 buckets"
-  value       = values(aws_s3_bucket.s3_buckets)[*].id
-}
-
-output "s3_bucket_arns" {
-  description = "ARNs of the S3 buckets"
-  value       = values(aws_s3_bucket.s3_buckets)[*].arn
 }
