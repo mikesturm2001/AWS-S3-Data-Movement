8417750560b2fd1c313c20aa22e8b41a8844ba35
Michael Sturm <mikesturm2001@yahoo.com>
2023-10-25 22:15:08 -0500

    rolling back changes

--- terraform/live/global/ecr/main.tf
+++ terraform/live/global/ecr/main.tf
@@ -18,26 +18,3 @@ resource "aws_ecr_repository" "aws_s3_data_movement_repository" {
   name = "aws_s3_data_movement_repository" 
 }
 
-# Define an ECR repository policy that allows only ECS tasks to pull images
-resource "aws_ecr_repository_policy" "ecr_policy" {
-  repository = aws_ecr_repository.aws_s3_data_movement_repository.name
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        },
-        Action = [
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetAuthorizationToken",
-          "ecr:GetImage",
-          "ecr:BatchGetImage",
-        ]
-      }
-    ]
-  })
-}
-
