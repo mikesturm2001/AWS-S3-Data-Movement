845b64a922261c4a7d38f7d941bc115f11b71438
Michael Sturm <mikesturm2001@yahoo.com>
2023-10-25 22:00:54 -0500

    updating ECR permissions to allow ecs tasks to pull images

--- terraform/live/global/ecr/main.tf
+++ terraform/live/global/ecr/main.tf
@@ -17,3 +17,28 @@ provider "aws" {
 resource "aws_ecr_repository" "aws_s3_data_movement_repository" {
   name = "aws_s3_data_movement_repository" 
 }
+
+# Create an ECR repository policy to allow ECS tasks to pull images
+data "aws_iam_policy_document" "ecr_repository_policy" {
+  statement {
+    actions   = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetAuthorizationToken",
+      "ecr:GetImage",
+      "ecr:BatchGetImage",
+    ]
+    resources = [aws_ecr_repository.aws_s3_data_movement_repository.arn]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+# Attach the ECR repository policy to the ECR repository
+resource "aws_ecr_repository_policy" "ecr_policy" {
+  repository  = aws_ecr_repository.aws_s3_data_movement_repository.name
+  policy      = data.aws_iam_policy_document.ecr_repository_policy.json
+}
+
